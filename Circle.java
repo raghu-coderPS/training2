1649657add2e11f2300bf8b0fac7022e2abbc1ad
raghu <raghuramkanagarla123@gmail.com>
2020-09-07 10:27:32 +0530

    added

---
+++ Circle.java
@@ -0,0 +1,21 @@
+package com.sapient.shapes;
+
+public class Circle{
+	public int Radius;
+	Circle()
+	{
+	
+	}
+	Circle(int Radius)
+	{
+	  this.Radius=Radius;	
+	}
+	void calculateAreaOfCircle(int Radius)
+	{
+		System.out.println(3.14*Radius*Radius);
+		/*changes made by person 1 */
+                System.out.println("Area of circle");
+
+	}
+
+}
